e2a0554c55fc4be9f17c5b4ef50ab4a77cef042f
cdinuta <cdinuta@axway.com>
2019-05-29 13:52:57 +0300

    modified render

--- render.py
+++ render.py
@@ -9,34 +9,40 @@ import jinja2
 import sys
 import yaml
 
-TEMPLATES_DIR = os.environ.get('TEMPLATES_DIR')
-TEMPLATE = os.environ.get('TEMPLATE')
-VARS_DIR = os.environ.get('VARS_DIR')
-VARIABLES = os.environ.get('VARIABLES')
+class Render:
 
-env = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(TEMPLATES_DIR),
-    extensions=['jinja2.ext.autoescape', 'jinja2.ext.do', 'jinja2.ext.loopcontrols', 'jinja2.ext.with_'],
-    autoescape=True,
-    trim_blocks=True)
+    def __init__(self):
+        pass
 
+    TEMPLATES_DIR = os.environ.get('TEMPLATES_DIR')
+    TEMPLATE = os.environ.get('TEMPLATE')
+    VARS_DIR = os.environ.get('VARS_DIR')
+    VARIABLES = os.environ.get('VARIABLES')
 
-def yamlFilter(value):
-    return yaml.dump(value, Dumper=yaml.RoundTripDumper, indent=4)
-
-def env_override(value, key):
-    return os.getenv(key, value)
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(TEMPLATES_DIR),
+        extensions=['jinja2.ext.autoescape', 'jinja2.ext.do', 'jinja2.ext.loopcontrols', 'jinja2.ext.with_'],
+        autoescape=True,
+        trim_blocks=True)
 
 
-def render_template(argv):
-    data = yaml.load(open(VARS_DIR + "/" + VARIABLES), Loader=yaml.Loader)
+    def yamlFilter(self, value):
+        return yaml.dump(value, Dumper=yaml.RoundTripDumper, indent=4)
 
-    env.filters['yaml'] = yamlFilter
-    env.globals["environ"] = lambda key: os.environ.get(key)
-    env.globals["get_context"] = lambda: data
+    def env_override(self, value, key):
+        return os.getenv(key, value)
 
-    template = env.get_template(TEMPLATE)
-    sys.stdout.write(template.render(data))
+
+    def rend_template(self, argv):
+        data = yaml.load(open(self.VARS_DIR + "/" + self.VARIABLES), Loader=yaml.Loader)
+
+        self.env.filters['yaml'] = self.yamlFilter
+        self.env.globals["environ"] = lambda key: os.environ.get(key)
+        self.env.globals["get_context"] = lambda: data
+
+        template = self.env.get_template(self.TEMPLATE)
+        sys.stdout.write(template.render(data))
 
 if __name__ == '__main__':
-    render_template(sys.argv[1:])
+    render = Render()
+    render.rend_template(sys.argv[1:])
