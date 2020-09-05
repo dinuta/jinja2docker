663521311752c4c65148522b7acad2fff2a5a61f
cdinuta <cdinuta@axway.com>
2020-09-05 23:42:36 +0300

    removed self

--- tests/render_test.py
+++ tests/render_test.py
@@ -12,6 +12,14 @@ class RenderTestCase(unittest.TestCase):
     def test_json(self):
         template_name = "json.j2"
         variables_path = "./inputs/variables/json.json"
+        render = Render(template_name=template_name, variables_path=variables_path)
+
+        rendered_data, data = RenderTestCase.get_data_and_rendered_data(render)
+        self.assertEqual(rendered_data, data)
+
+    def test_yml(self):
+        template_name = "yml.j2"
+        variables_path = "./inputs/variables/yml.yml"
         r = Render(template_name=template_name, variables_path=variables_path)
 
         rendered_data, data = self.get_data_and_rendered_data(r)
@@ -22,14 +30,6 @@ class RenderTestCase(unittest.TestCase):
         with open(r.variables_path, closefd=True) as f:
             data = yaml.safe_load(f)
         return rendered_data, data
-
-    def test_yml(self):
-        template_name = "yml.j2"
-        variables_path = "./inputs/variables/yml.yml"
-        r = Render(template_name=template_name, variables_path=variables_path)
-
-        rendered_data, data = self.get_data_and_rendered_data(r)
-        self.assertEqual(rendered_data, data)
 
     def test_main_no_parameters(self):
         try:
